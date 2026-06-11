da2cadebae56bc3ebeeab607144046b5e97d9abe
unknown <bhattmanthank@gmail.com>
2026-06-11 13:52:46 +0530

    Fix Neon pooled connection

--- db.py
+++ db.py
@@ -5,12 +5,12 @@ import streamlit as st
 def get_conn():
     return psycopg2.connect(
         st.secrets["DATABASE_URL"],
-        cursor_factory=psycopg2.extras.RealDictCursor,
-        options="-c search_path=chronos_ml"
+        cursor_factory=psycopg2.extras.RealDictCursor
     )
 
 def query(sql, params=None):
     with get_conn() as conn:
         with conn.cursor() as cur:
+            cur.execute("SET search_path TO chronos_ml")
             cur.execute(sql, params or ())
             return cur.fetchall()
