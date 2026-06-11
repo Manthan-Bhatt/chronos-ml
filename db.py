d847fabddcf6433a739145eb7633d0aeb427f661
unknown <bhattmanthank@gmail.com>
2026-06-11 13:44:44 +0530

    Using conn string

--- db.py
+++ db.py
@@ -4,12 +4,7 @@ import streamlit as st
 
 def get_conn():
     return psycopg2.connect(
-        host=st.secrets["DB_HOST"],
-        dbname=st.secrets["DB_NAME"],
-        user=st.secrets["DB_USER"],
-        password=st.secrets["DB_PASS"],
-        port=5432,
-        sslmode="require",
+        st.secrets["DATABASE_URL"],
         cursor_factory=psycopg2.extras.RealDictCursor,
         options="-c search_path=chronos_ml"
     )
